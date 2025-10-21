a4440f22a1866767b4842b4e6fdb98f39adfe0ee
MarcAnthonyWilliams <mw4725@nyu.edu>
2025-10-21 00:32:46 -0400

    feat: added CRUD for postgres

--- src/docker/init-db.sql
+++ src/docker/init-db.sql
@@ -5,6 +5,7 @@ CREATE TABLE profiles.users(
     username VARCHAR(50) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     role user_role NOT NULL,
+    default_collection VARCHAR(100),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )
--- New table soon for queries. 
+-- New table soon for queries, and userqueries(6) should ref this table. 
